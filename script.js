8c0cd1d40908046a7a3c98f9d3619877d0a00aad
Th3Spacepope <tun44465@temple.edu>
2025-02-02 20:07:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 //↓↓↓↓↓↓↓↓↓↓↓↓YOUR CODE goes below here↓↓↓↓↓↓↓↓↓↓↓↓
-
-
+class MIDIengine {
+  constructor(input, output) {
+    MIDIin = input;
+    MIDIout = output;
+    running = false;
+  }
+  enable(on_off) {
+    this.running = on_off;
+    console.log(`The MIDI Engine is now on`);
+  }
+}
 
 //↑↑↑↑↑↑↑↑↑↑YOUR CODE goes above here↑↑↑↑↑↑↑↑↑↑
 //Do not edit below this comment!
-const testMIDI = new MIDIengine("MAudio", "PreSonus Interface")
-const testMIDI2 = new MIDIengine("OP-1","MOTU")
+const testMIDI = new MIDIengine("MAudio", "PreSonus Interface");
+const testMIDI2 = new MIDIengine("OP-1", "MOTU");
