79b4c1cc9455b74b7d932daf8dd10f47357e957f
Th3Spacepope <tun44465@temple.edu>
2025-02-04 10:48:19 -0500

    Still broken

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 //↓↓↓↓↓↓↓↓↓↓↓↓YOUR CODE goes below here↓↓↓↓↓↓↓↓↓↓↓↓
 class MIDIengine {
   constructor(input, output) {
-    MIDIin = input;
-    MIDIout = output;
-    running = false;
+    this.MIDIin = input;
+    this.MIDIout = output;
+    this.running = false;
   }
   enable(on_off) {
     this.running = on_off;
-    console.log(`The MIDI Engine is now on`);
+    if ((this.running = true)) {
+      console.log(`The MIDI Engine is now on`);
+    } else {
+      console.log("The MIDIengine is now off");
+    }
+  }
+  printInfo() {
+    console.log(
+      `The MIDIengine has an input of ${this.MIDIin}, and an output of ${this.MIDIout}.`
+    );
   }
 }
 
